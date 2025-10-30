fd3994b7ec790cda521ae11f43390d207a825344
Gyanendra <pgyanendra333@gmail.com>
2025-10-30 18:20:44 +0530

    route

--- 03_admin/src/App.tsx
+++ 03_admin/src/App.tsx
@@ -1,8 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
