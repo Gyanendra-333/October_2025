c555128863b3015b7ae36f47d585cd174adcc8b7
Gyanendra <pgyanendra333@gmail.com>
2025-11-04 15:59:22 +0530

    message

--- 05 Chat App/server/controller/message.controller.js
+++ 05 Chat App/server/controller/message.controller.js
@@ -1,6 +1,7 @@
 import { catchAsyncError } from "../middleware/catchAsyncError.middleware.js"
 import { Message } from "../models/message.model.js";
 import { User } from "../models/user.model.js";
+import { v2 as cloudinary } from "cloudinary"
 
 // Get All User 
 export const getAllUsers = catchAsyncError(async (req, res, next) => {
@@ -41,4 +42,51 @@ export const getMessage = catchAsyncError(async (req, res, next) => {
 
 
 // Send Message 
-export const sendMessage = catchAsyncError(async (req, res, next) => { })
+export const sendMessage = catchAsyncError(async (req, res, next) => {
+    const { text } = req.body;
+    const media = req?.files?.media;
+    const { id: recieverId } = req.params;
+    const senderId = req.user._id;
+
+    const receiver = await User.findById(recieverId);
+    if (!receiver) {
+        return res.status(400).json({
+            success: false,
+            message: "Receiver Id invalid"
+        })
+    }
+
+    const sanitizeText = text?.trim() || "";
+    if (!sanitizeText || !media) {
+        return res.status(400).json({
+            success: false,
+            message: "can't send empty message"
+        })
+    }
+
+    let mediaUrl = "";
+    if (media) {
+        try {
+            const uploadResponse = await cloudinary.uploader.upload(
+                media.tempfilepath,
+                {
+                    resource_type: "auto",
+                    folder: "CHAT_APP",
+                    transformation: [
+                        { width: 720, height: 720, crop: "limit" },
+                        { quality: "auto" },
+                        { fetch_format: "auto" }
+                    ]
+                }
+            )
+            mediaUrl = uploadResponse?.secure_url;
+        } catch (error) {
+            console.log("Cloudinary upload error", error)
+            return res.status(400).json({
+                success: false,
+                message: "Failed to upload media.", error
+            })
+        }
+    }
+
+});
