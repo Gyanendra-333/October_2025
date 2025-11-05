3926825a8c0ed640bb827801c94d71a0cb7f9d2a
Gyanendra <pgyanendra333@gmail.com>
2025-11-05 17:55:00 +0530

    done

--- 05 Chat App/client/src/App.jsx
+++ 05 Chat App/client/src/App.jsx
@@ -4,6 +4,14 @@ import { useSelector, useDispatch } from "react-redux"
 import { useEffect } from "react";
 import { getUser, setOnlineUsers } from "./store/slices/authSlice";
 import { connectSocket, disconnectSocket } from "./lib/socket";
+import { Loader } from "lucide-react";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import { ToastContainer } from "react-toastify"
 
 
 const App = () => {
@@ -24,9 +32,26 @@ const App = () => {
     }
   }, [authUser])
 
-  return <div>
-    App
-  </div>;
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    )
+  }
+
+  return <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+        <Route path="/register" element={!authUser ? <Register /> : <Navigate to={"/"} />} />
+        <Route path="/login" element={!authUser ? <Login /> : <Navigate to={"/"} />} />
+        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to={"/login"} />} />
+      </Routes>
+      <ToastContainer />
+    </Router>
+  </>;
 };
 
 export default App;
