82db490667cd4864ed12a6d81ff998f30b0e0029
Gyanendra <pgyanendra333@gmail.com>
2025-11-05 12:53:38 +0530

    socket setup

--- 05 Chat App/client/src/App.jsx
+++ 05 Chat App/client/src/App.jsx
@@ -1,8 +1,32 @@
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
-import { Loader } from "lucide-react";
+import { useSelector, useDispatch } from "react-redux"
+import { useEffect } from "react";
+import { getUser, setOnlineUsers } from "./store/slices/authSlice";
+import { connectSocket, disconnectSocket } from "./lib/socket";
+
+
 const App = () => {
-  return <></>;
+  const { authUser, isCheckingAuth } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [getUser])
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = connectSocket(authUser._id);
+      socket.on("setOnlineUsers", (users) => {
+        dispatch(setOnlineUsers(users))
+      })
+      return () => disconnectSocket();
+    }
+  }, [authUser])
+
+  return <div>
+    App
+  </div>;
 };
 
 export default App;
--- 05 Chat App/client/src/store/store.js
+++ 05 Chat App/client/src/store/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "@reduxjs/toolkit";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
 
 export default store;
