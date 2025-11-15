1a7eb8913a3afaadbe7a96042f6862045bd41094
Gyanendra <pgyanendra333@gmail.com>
2025-11-15 17:16:37 +0530

    creaet validation and schema

--- 07_nexxxt/models/User.model.ts
+++ 07_nexxxt/models/User.model.ts
@@ -23,7 +23,48 @@ export interface User extends Document {
     email: string,
     password: string,
     verifyCode: string,
+    isVerified: boolean,
     verifyCodeExpiry: Date,
     isAcceptingMessage: boolean,
-    message: Message[]
+    messages: Message[]
 };
+
+
+const UserSchema: Schema<User> = new Schema({
+    userName: {
+        type: String,
+        required: [true, "userName is required"],
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: true,
+        trim: true,
+        match: [/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, "Please enter a valid email"]
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "verify Code is required"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "verify Expiry Code is required"],
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true
+    },
+    messages: [MessageSchema]
+
+
+});
+ 
+const userModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>("User", UserSchema));
+
+export default userModel;
---
+++ 07_nexxxt/schema/signUpSchema.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+// userNameValidationSchema
+export const userNameValidationSchema = z
+    .string()
+    .min(3, "Username must be at least 3 characters")
+    .max(20, "Username must be under 20 characters")
+    .regex(/^[A-Za-z][A-Za-z0-9_]*$/, "Username must start with a letter and contain only letters, numbers, or underscores");
+
+
+// signUpSchema
+export const signUpSchema = z
+    .object({
+        username: userNameValidationSchema,
+
+        email: z
+            .string()
+            .email("Invalid email address"),
+
+        password: z
+            .string()
+            .min(6, "Password must be at least 6 characters")
+            .max(32, "Password must not exceed 32 characters"),
+
+        confirmPassword: z
+            .string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+    });
