013daea03d2ec69b0c92d3e422232a427bbef353
Gyanendra <pgyanendra333@gmail.com>
2025-12-09 17:28:08 +0530

    python learn

--- 11 python/hello.py
+++ 11 python/hello.py
@@ -1,3 +1,28 @@
 
+# # # print("hello world")
 
-print("hello world")
+# # # # commenting in python
+
+# # # name = "Gyanendra"
+# # # print(name)
+
+
+# # # # String
+# # # name = "Gyanendra"
+# # # print(name)
+
+# # string_name = "Gyanendra as String"
+# # print(len(string_name))
+# # print(string_name[3])
+# # print(string_name[-3])
+
+# # print(string_name[0:3])
+
+
+# print("This is testing message 'check' message")
+# print('This is testing message "check"  message')
+
+name = "Gyanendra"
+print(name.upper())
+print(name.lower())
+print(name.find("y"))
