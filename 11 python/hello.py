2a378e95717f03be868212dac83fe4187fb4ec97
Gyanendra <pgyanendra333@gmail.com>
2025-12-10 14:33:11 +0530

    python basics

--- 11 python/hello.py
+++ 11 python/hello.py
@@ -1,28 +1,45 @@
 
-# # # print("hello world")
+# # # # # # print("hello world")
 
-# # # # commenting in python
+# # # # # # # commenting in python
+
+# # # # # # name = "Gyanendra"
+# # # # # # print(name)
+
+
+# # # # # # # String
+# # # # # # name = "Gyanendra"
+# # # # # # print(name)
+
+# # # # # string_name = "Gyanendra as String"
+# # # # # print(len(string_name))
+# # # # # print(string_name[3])
+# # # # # print(string_name[-3])
+
+# # # # # print(string_name[0:3])
+
+
+# # # # print("This is testing message 'check' message")
+# # # # print('This is testing message "check"  message')
 
 # # # name = "Gyanendra"
-# # # print(name)
+# # # print(name.upper())
+# # # print(name.lower())
+# # # print(name.find("y"))
 
 
-# # # # String
-# # # name = "Gyanendra"
-# # # print(name)
-
-# # string_name = "Gyanendra as String"
-# # print(len(string_name))
-# # print(string_name[3])
-# # print(string_name[-3])
-
-# # print(string_name[0:3])
+# # import math
 
 
-# print("This is testing message 'check' message")
-# print('This is testing message "check"  message')
+# # num = 2.1
+# # print(math.ceil(num))
 
-name = "Gyanendra"
-print(name.upper())
-print(name.lower())
-print(name.find("y"))
+
+# print()
+# input()
+
+
+name = 5
+name2 = "5"
+result = name+"2"
+print(result)
