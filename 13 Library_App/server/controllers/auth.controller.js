aa93de2dd021f0ed6c59eed4f8d4a500dc4b951b
Gyanendra <pgyanendra333@gmail.com>
2025-12-18 11:55:31 +0530

    register controller

--- 13 Library_App/server/controllers/auth.controller.js
+++ 13 Library_App/server/controllers/auth.controller.js
@@ -1,2 +1,33 @@
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/catchAsyncError";
 
 
+export const register = asyncHandler(async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return next(new ErrorHandler("Please provide all required fields", 400));
+        }
+        const isRegistered = await User.findOne({ email });
+        if (isRegistered) {
+            return next(new ErrorHandler("User already registered. Please login.", 409));
+        }
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name,
+            email,
+            password: hashPassword,
+        });
+        await user.save();
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            user,
+        });
+
+
+    } catch (error) {
+        next(error);
+        console.log("Error in register controller:", error);
+    }
+});
