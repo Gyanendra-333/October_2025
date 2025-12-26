5887b65e5bce79a05d96d3c85d27128afebd7ce8
Gyanendra <pgyanendra333@gmail.com>
2025-12-26 11:52:55 +0530

    routes

--- 13 Library_App/server/routes/user.route.js
+++ 13 Library_App/server/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { login, logout, register } from "../controllers/auth.controller.js";
+import { forgotPassword, login, logout, register, resetPassword } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/forgetPassword", forgotPassword);
+router.post("/resetPassword/:token", resetPassword);
 
 export default router;
